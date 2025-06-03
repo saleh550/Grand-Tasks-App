db0615f92a0fe63bca52a0e6189b7a415b3065aa
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2025-06-03 09:02:30 +0300

    completer added

--- lib/screens/ThirdScreen.dart
+++ lib/screens/ThirdScreen.dart
@@ -1,6 +1,6 @@
 // screens/home_screen.dart
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 class ThirdScreen extends StatefulWidget  {
   @override
 _Task3ScreenState createState()=>_Task3ScreenState();
@@ -10,31 +10,34 @@ class _Task3ScreenState extends State<ThirdScreen> {
   int currentNumber = 0;
   bool isProcessing = false;
   bool isEvenTurn = true;
+  Completer<void>? evenCompleter;
+  Completer<void>? oddCompleter;
   // Even numbers
   Future<void> generateEvenNumbers() async {
     for (int i = 0; i < 100; i += 2) {
-      while (!isEvenTurn) {
-        await Future.delayed(Duration(milliseconds: 10));
-      }
+      await evenCompleter!.future;
       await Future.delayed(Duration(milliseconds: 500));
       setState(() {
         currentNumber=i;
       });
-      isEvenTurn = false;
+      evenCompleter = Completer<void>(); // reset
+      oddCompleter!.complete(); // signal odd thread
     }
   }
 
   // Odd numbers
   Future<void> generateOddNumbers() async {
     for (int i = 1; i < 100; i += 2) {
-      while (isEvenTurn) {
-        await Future.delayed(Duration(milliseconds: 10));
-      }
-      await Future.delayed(Duration(milliseconds: 500));
+      await oddCompleter!.future;
+
+      await Future.delayed(Duration(milliseconds: 500)); // simulate work
       setState(() {
         currentNumber=i;
       });
       isEvenTurn = true;
+
+      oddCompleter = Completer<void>(); // reset
+      evenCompleter!.complete(); // signal even thread
     }
   }
 
@@ -46,7 +49,12 @@ class _Task3ScreenState extends State<ThirdScreen> {
       isProcessing = true;
     });
 
+    // Initialize completers
+    evenCompleter = Completer<void>();
+    oddCompleter = Completer<void>();
 
+    // Start with even
+    evenCompleter?.complete();
     await Future.wait([
       generateEvenNumbers(),
       generateOddNumbers(),
